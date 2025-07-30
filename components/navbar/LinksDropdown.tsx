c5d28c01634a0098183fabcbc94c4ec103a020fe
majidH <majid.hadadian@gmail.com>
2025-07-30 12:03:38 +0330

    add authentication dropdown with sign in/up/out using Clerk

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -2,14 +2,17 @@ import { links } from "@/utils/link";
 import UserIcon from "./UserIcon";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import { LuAlignLeft } from "react-icons/lu";
 
 import {
+  DropdownMenu,
+  DropdownMenuSeparator,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import SignOutLink from "./SignOutLink";
 
 function LinksDropdown() {
   return (
@@ -23,16 +26,38 @@ function LinksDropdown() {
           <UserIcon />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-52" align="start" sideOffset={10}>
-        {links.map((link) => {
-          return (
-            <DropdownMenuItem asChild key={link.href}>
-              <Link href={link.href} className="capitalize w-full">
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          );
-        })}
+      <DropdownMenuContent className="w-50" align="start" sideOffset={10}>
+        {/* Sign Out */}
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode="modal">
+              <button className="w-full text-left">Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode="modal">
+              <button className="w-full text-left">Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+
+        {/* Sign Out */}
+         <SignedIn>
+      {links.map((link) => {
+        return (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className='capitalize w-full'>
+              {link.label}
+            </Link>
+          </DropdownMenuItem>
+        );
+      })}
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>
+        <SignOutLink />
+      </DropdownMenuItem>
+    </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,6 +1,23 @@
+"use client";
+
+import { useClerk } from "@clerk/nextjs";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+
 function SignOutLink() {
+  const { signOut } = useClerk();
+
+  const handleLogout = async () => {
+    await signOut();
+    toast("You have been signed out.");
+    window.location.href = "/";
+  };
+
   return (
-    <div>SignOutLink</div>
-  )
+    <Button variant='default' size='sm' onClick={handleLogout} className="w-full cursor-pointer">
+      Logout
+    </Button>
+  );
 }
-export default SignOutLink
+
+export default SignOutLink;
