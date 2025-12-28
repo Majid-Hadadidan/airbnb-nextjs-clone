fd51bf1e67b29628ad9a80195a4dcc9d52e53f94
majidH <majid.hadadian@gmail.com>
2025-12-28 09:02:05 +0330

    feat:add proertyList component  and insert PropertyCard component

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,4 +1,13 @@
-function PropertiesList() {
-  return <div>PropertiesList</div>;
+import type { PropertyCardProps } from "@/utils/types";
+import PropertyCard from "../card/PropertyCard";
+
+function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
+  return (
+    <section>
+      {properties.map((property) => {
+        return <PropertyCard key={property.id} property={property} />;
+      })}
+    </section>
+  );
 }
 export default PropertiesList;
