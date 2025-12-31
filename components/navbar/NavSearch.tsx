b2e4c46164365039470d08f97c303920b92acb3f
majidH <majid.hadadian@gmail.com>
2025-12-31 12:35:52 +0330

    feat(navbar): add debounced search input with URL sync

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,8 +1,43 @@
-import { Input } from "../ui/input"
-
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Input } from "../ui/input";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 function NavSearch() {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+  const hanleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathName}?${params.toString()}`);
+  }, 500);
+
+  //useEffect
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
   return (
-    <Input type="text" placeholder="find a property..." className="max-w-xs dark:bg-muted"/>
-  )
+    <Input
+      type="text"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted"
+      onChange={(e) => {
+        setSearch(e.target.value);
+        hanleSearch(e.target.value);
+      }}
+      value={search}
+    />
+  );
 }
-export default NavSearch
+export default NavSearch;
