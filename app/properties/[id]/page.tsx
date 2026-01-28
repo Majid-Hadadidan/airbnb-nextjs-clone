7824d20821b4d4b70fa4a1883a8e08c678958e1a
majidH <majid.hadadian@gmail.com>
2026-01-28 14:03:05 +0330

    feat:create Calendar component and add to PropertyDetails page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
+import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
 import ShareButton from "@/components/properties/ShareButton";
@@ -31,7 +32,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           </div>
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
-          {/* calender */}
+          <BookingCalendar/>
         </div>
       </section>
     </section>
